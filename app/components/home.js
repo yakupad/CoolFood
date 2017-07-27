07d0a3f63c041d30aec4b8d633e1d340732de7fc
yakupad <yakupad@yandex.com>
2017-07-27 22:41:22 +0300

    TabBar Added

--- app/components/home.js
+++ app/components/home.js
@@ -1,39 +1,66 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
-import React, { Component } from 'react';
+import React from 'react';
 import {
-  AppRegistry,
   StyleSheet,
   Text,
   View,
-  Button,
-  Navigator,
-  Image,
-  TextInput
+  ScrollView,
 } from 'react-native';
 
-export default class Home extends Component {
+import FacebookTabBar from './helper/facebookTabBar';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+
+export default React.createClass({
   render() {
-    return (
-      
-        <View style={styles.container}>
-            <Text>Deneme</Text>
-            </View>
-     
-    );
-  }
-}
+    return <ScrollableTabView
+      style={{marginTop: 20, }}
+      initialPage={1}
+      renderTabBar={() => <FacebookTabBar />}
+      >
+      <ScrollView tabLabel="ios-paper" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>News</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-people" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Friends</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-chatboxes" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Messenger</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-notifications" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Notifications</Text>
+        </View>
+      </ScrollView>
+      <ScrollView tabLabel="ios-list" style={styles.tabView}>
+        <View style={styles.card}>
+          <Text>Other nav</Text>
+        </View>
+      </ScrollView>
+    </ScrollableTabView>;
+  },
+});
 
 const styles = StyleSheet.create({
-  container: {
+  tabView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  }
-  
+    padding: 10,
+    backgroundColor: 'rgba(0,0,0,0.01)',
+  },
+  card: {
+    borderWidth: 1,
+    backgroundColor: '#fff',
+    borderColor: 'rgba(0,0,0,0.1)',
+    margin: 5,
+    height: 150,
+    padding: 15,
+    shadowColor: '#ccc',
+    shadowOffset: { width: 2, height: 2, },
+    shadowOpacity: 0.5,
+    shadowRadius: 3,
+  },
 });
--- index.ios.js
+++ index.ios.js
@@ -23,7 +23,7 @@ export default class CoolFood extends Component {
   render() {
     return (
       
-        <Home />
+        <Signup />
      
     );
   }
